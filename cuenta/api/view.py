2d946de671f1d70a5dd11667166b4e9ff4e341b0
juan jose <juan.jara@campusucc.edu.co>
2023-04-11 20:06:08 -0500

    agregacion permisos

--- cuenta/api/view.py
+++ cuenta/api/view.py
@@ -3,12 +3,14 @@ from cuenta.models import Cuenta
 from cuenta.api.serializers import CuentaSerializer
 from cuenta.api.filters import CuentasFilters
 from django_filters.rest_framework import DjangoFilterBackend
+from cuenta.api.permissions import isAdminOrReadOnly
 
 class CuentaApiViewSet(ModelViewSet):
     serializer_class= CuentaSerializer
     queryset = Cuenta.objects.all()
-
+    permission_classes = [isAdminOrReadOnly]
     # queryset = Cuenta.objects.filter(saldo_disponible__gte=250000)
     filter_backends=[DjangoFilterBackend]
     filterset_class=CuentasFilters
+
     
